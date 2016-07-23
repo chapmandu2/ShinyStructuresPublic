5f0b76acf9e16b26ccd5f6859070e32721434af0
Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
2016-07-23 19:08:57 +0100

    Modified to render example data structures by default

--- server.R
+++ server.R
@@ -11,23 +11,36 @@ shinyServer(function(input, output) {
             if('data.frame' %in% class(out)) {
                 return(out)
             } else {
-                return(getData())
+                return(NULL)
             }
+    })
+    
+    data <- reactive({
+        if(!is.null(import_data())){
+            import_data()  
+        } else {
+            getData() 
+        }  
     })
     
     process_data <- reactive({
         
-        dat <- import_data()
-        structure_col <- input$col_select
-        
-        if (!is.null(structure_col)) {
-            if(structure_col != 'none') {
-                dat <- dat %>% 
-                    dplyr::rename_(sel_col=structure_col) %>%
-                    dplyr::mutate(structure=makeChemDoodleMolfile(sel_col, row_number())) %>%
-                    dplyr::select(-sel_col)
-        }}
-        return(dat)
+        #do we have custom data? 
+        custom_data_lgl <- !is.null(import_data())  
+        #either import the custom data or get the example data
+        dat <- data()
+        #consider NULL and none the same
+        structure_col <- ifelse(is.null(input$col_select), 'none', input$col_select) 
+        #if we don't have custom data, and no col is selected, specify MOLFILE as structure col
+        structure_col <- ifelse(!custom_data_lgl & structure_col == 'none', 'MOLFILE', structure_col) 
+ 
+        if(structure_col != 'none') {
+            dat <- dat %>% 
+                dplyr::rename_(sel_col=structure_col) %>%
+                dplyr::mutate(structure=makeChemDoodleMolfile(sel_col, row_number())) %>%
+                dplyr::select(-sel_col)
+    }
+    return(dat)
     })
     
     output$dt <- DT::renderDataTable({
@@ -36,7 +49,7 @@ shinyServer(function(input, output) {
             })
     
     output$col_select <- renderUI({
-        selectInput('col_select', 'Select Molfile Column', choices=c('none', colnames(import_data())), selected='none')
+        selectInput('col_select', 'Select Molfile Column', choices=c('none', colnames(data())), selected='none')
         
     })
     
